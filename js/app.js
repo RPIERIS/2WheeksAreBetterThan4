ca54bfdbea579af24cdef5d4d8ee329c5225254b
Ranjana Pieris <rpieris@yahoo.co.uk>
2020-11-10 05:04:28 +0000

    Initial javascript added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
-// create javascript code here 
+// create javascript code here
+const contactSelect = document.querySelector('.contact');
+const contactLoc = document.getElementById('contact')
+
+contactSelect.addEventListener('click', function(){
+  contactLoc.scrollIntoView({
+    behaviour: "smooth"
+  });
+});
+
+const dirtbikeSelect = document.querySelector('.dirt-bike');
+const dirtbikeLoc = document.getElementById('dirt-bike')
+
+dirtbikeSelect.addEventListener('click', function(){
+  dirtbikeLoc.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 

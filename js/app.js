b0076fac75472336e03e834eb6324effb13ffd01
Ranjana Pieris <rpieris@yahoo.co.uk>
2020-11-10 05:50:16 +0000

    javascript to navbar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-// create javascript code here
+//--------------------
+// navbar and sections
+//--------------------
+
+// I have written this section but I think there might
+// a simpler way with less code to get to the relevant
+// sections
+
 const contactSelect = document.querySelector('.contact');
 const contactLoc = document.getElementById('contact')
 
@@ -8,37 +15,61 @@ contactSelect.addEventListener('click', function(){
   });
 });
 
-const dirtbikeSelect = document.querySelector('.dirt-bike');
-const dirtbikeLoc = document.getElementById('dirt-bike')
+const roadBikeSelect = document.querySelector('.road-bike');
+const roadBikeLoc = document.getElementById('road-bike')
 
-dirtbikeSelect.addEventListener('click', function(){
-  dirtbikeLoc.scrollIntoView({
+roadBikeSelect.addEventListener('click', function(){
+  roadBikeLoc.scrollIntoView({
+    behaviour: "smooth"
+  });
+});
+
+const touringBikeSelect = document.querySelector('.touring-bike');
+const touringBikeLoc = document.getElementById('touring-bike')
+
+touringBikeSelect.addEventListener('click', function(){
+  touringBikeLoc.scrollIntoView({
+    behaviour: "smooth"
+  });
+});
+
+const hybridBikeSelect = document.querySelector('.hybrid-bike');
+const hybridBikeLoc = document.getElementById('hybrid-bike')
+
+hybridBikeSelect.addEventListener('click', function(){
+  hybridBikeLoc.scrollIntoView({
+    behaviour: "smooth"
+  });
+});
+
+const dirtBikeSelect = document.querySelector('.dirt-bike');
+const dirtBikeLoc = document.getElementById('dirt-bike')
+
+dirtBikeSelect.addEventListener('click', function(){
+  dirtBikeLoc.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
+const dutchBikeSelect = document.querySelector('.dutch-bike');
+const dutchBikeLoc = document.getElementById('dutch-bike')
+
+dutchBikeSelect.addEventListener('click', function(){
+  dutchBikeLoc.scrollIntoView({
     behaviour: 'smooth'
   })
 })
 
 
+//--------------------
+// back to to button
+//--------------------
 
+// below code was taken and adaoted frin w3school
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -49,8 +80,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

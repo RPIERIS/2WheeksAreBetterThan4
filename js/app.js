3025172cc0a4a46f02b59702c6168a70678ec9ac
Ranjana Pieris <rpieris@yahoo.co.uk>
2020-11-12 14:56:16 +0000

    hidden navbar

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ contactSelect.addEventListener('click', function(){
   });
 });
 
+
 const roadBikeSelect = document.querySelector('.road-bike');
 const roadBikeLoc = document.getElementById('road-bike')
 
@@ -23,6 +24,13 @@ roadBikeSelect.addEventListener('click', function(){
     behaviour: "smooth"
   });
 });
+
+roadBikeLoc.addEventListener("mouseover", function(event) {
+  setTimeout (function(){
+  roadBikeSelect.style.background = "rgb(208, 219, 159)";
+},1000);
+});
+
 
 const touringBikeSelect = document.querySelector('.touring-bike');
 const touringBikeLoc = document.getElementById('touring-bike')
@@ -62,22 +70,26 @@ dutchBikeSelect.addEventListener('click', function(){
 
 
 //--------------------
-// back to to button
+// back to top button
 //--------------------
 
-// below code was taken and adaoted frin w3school
+// below code was taken and adaopted frin w3school
 //https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 mybutton = document.getElementById("myBtn");
+myNav = document.getElementById("navBar");
 
 window.onscroll = function() {scrollFunction()};
+// window.onmousemove = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    myNav.style.display = "flex";
     setTimeout (function(){
       mybutton.style.display = "none";
-    }, 5000);
+      myNav.style.display = "none";
+    }, 10000);
   } else {
     mybutton.style.display = "none";
   }
@@ -86,9 +98,14 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+  myNav.style.display = "flex";
 }
 
 function timedbutton() {
   const x = document.getElementById("myBtn");
-  setTimeout(function(){x.style.display = "none"}, 2000);
+  setTimeout(function(){x.style.display = "none"}, 10000);
 }
+
+myNav.addEventListener("mouseover", function(event) {
+  myNav.style.display = "flex";
+},false);

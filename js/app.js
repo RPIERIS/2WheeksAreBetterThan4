8448cf9db907136b988abfd2acae5ec8255081ee
Ranjana Pieris <rpieris@yahoo.co.uk>
2020-11-24 08:38:55 +0000

    before deleteing redundant javascript

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 //--------------------
 
 // code was refactored to go to a specific section
+// via the nave items
 
 const navSelect = document.getElementById('navBar');
 
@@ -12,29 +13,33 @@ navSelect.addEventListener('click',function(e){
   contactNewLoc.scrollIntoView();
 });
 
-//--------------------
-// back to top button
-//--------------------
+//----------------------------------
+// back to top button & Hide Navbar
+//----------------------------------
 
-// below code was taken and adapted frin w3school
+// below code was taken and adapted from w3school
 //https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 mybutton = document.getElementById("myBtn");
 myNav = document.getElementById("navBar");
 
 window.onscroll = function() {scrollFunction()};
+// the below cose was commented out as it beame unstable
+// further clarity is needed in how the bubbling works
 // window.onmousemove = function() {scrollFunction()};
+// the timeout was not firing correctly
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (window.scrollY >= 44) {
     mybutton.style.display = "block";
     myNav.style.display = "flex";
-    setTimeout (function(){
-      mybutton.style.display = "none";
-      myNav.style.display = "none";
-    }, 10000);
+    // setTimeout (function(){
+    //   mybutton.style.display = "none";
+    //   myNav.style.display = "none";
+    // }, 10000);
   } else {
     mybutton.style.display = "none";
+    myNav.style.display = "flex";
   }
 }
 
@@ -50,13 +55,64 @@ function timedbutton() {
   setTimeout(function(){x.style.display = "none"}, 10000);
 }
 
-myNav.addEventListener("mouseover", function(event) {
-  myNav.style.display = "flex";
-},false);
+// change navigation style on scroll
+window.addEventListener('scroll', event => {
+  let nav = document.querySelector('.navbar');
+  // console.log(nav);
+  (window.scrollY >= 44) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+})
+
+// active navigation on scroll
+window.addEventListener('scroll', event => {
+  let navigationLinks = document.querySelectorAll('nav div');
+  console.log(navigationLinks);
+  let fromTop = window.scrollY;
+
+  navigationLinks.forEach(link => {
+    let section = document.querySelector(link.classList);
+    console.log(section);
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  })
+})
+
+// --------------------------------
+// show nav & button on mouse click
+// --------------------------------
 
 document.onclick = function(e) {
   mybutton.style.display = "block";
   myNav.style.display = "flex";
-  // let coords = e.clientX + ":" + e.clientY;
-  // console.log(coords);
 }
+
+// ----------------------------------------
+// display section on navbar when scrolling
+// ----------------------------------------
+
+// const sectionSelect = document.getElementsByTagName('MAIN');
+//
+// sectionSelect.addEventListener('scroll', function(e){
+//   let sectionName = e.target.className;
+//   console.log(sectionName);
+// })
+
+const sectionSelect = document.getElementById('mainSection')
+// console.log(sectionSelect);
+sectionSelect.addEventListener('scroll',function(e){
+   console.log(e.target.classname);
+  });
+
+const roadBikeSelect = document.querySelector('.road-bike');
+const roadBikeLoc = document.getElementById('road-bike')
+
+roadBikeLoc.addEventListener("mouseover", function(event) {
+  setTimeout (function(){
+  roadBikeSelect.style.background = "rgb(208, 219, 159)";
+},1000);
+});
